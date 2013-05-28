0a5e8174d651d02c46e098aa3e4a4584e3dea867
DrMartiner <DrMartiner@GMail.Com>
2013-05-28 16:27:17 +0700

    Add sms_id at SMSMEssage

--- smsaero/factoey.py
+++ smsaero/factoey.py
@@ -8,14 +8,13 @@ from .models import SMSMessage
 class SignatureF(factory.Factory):
     FACTORY_FOR = Signature
 
-    # id = factory.Sequence(lambda n: n)
     name = factory.Sequence(lambda n: 'Name{0}'.format(n))
 
 
 class SMSMessageF(factory.Factory):
     FACTORY_FOR = SMSMessage
 
-    # id = factory.Sequence(lambda n: n)
+    sms_id = factory.Sequence(lambda n: n)
     phone = factory.Sequence(lambda n: '7123456789{0}'.format(n))
     signature = factory.LazyAttribute(lambda a: SignatureF())
     text = factory.Sequence(lambda n: 'Message{0}'.format(n))
