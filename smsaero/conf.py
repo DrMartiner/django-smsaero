a0ad0480551070ff911f58fc2ba8cadb1ccf5243
DrMartiner <DrMartiner@GMail.Com>
2013-05-29 23:24:12 +0700

    Add param SMSAERO_ASYNC

--- smsaero/conf.py
+++ smsaero/conf.py
@@ -5,3 +5,4 @@ from django.conf import settings
 SMSAERO_USER = getattr(settings, 'SMSAERO_USER')
 SMSAERO_PASSWORD = getattr(settings, 'SMSAERO_PASSWORD', '')
 SMSAERO_PASSWORD_MD5 = getattr(settings, 'SMSAERO_PASSWORD_MD5', '')
+SMSAERO_ASYNC = getattr(settings, '§', False)
